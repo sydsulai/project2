4d61ad6aaa440bad65626dd322828154506e8bd4
sydsulai <60973972+sydsulai@users.noreply.github.com>
2022-05-07 09:25:50 +0530

    Update creds.tf
    Removing credentials

--- terraform-section/creds.tf
+++ terraform-section/creds.tf
@@ -1,12 +1,11 @@
 provider "aws" {
 
-  access_key = "ASIARAXMKJ7WQ557ASM5"
+  access_key = ""
 
-  secret_key = "94vEwowF0AnIMq2P0B0ji7znqpMov4O8wBWY9ZtG"
+  secret_key = ""
 
-  token = "FwoGZXIvYXdzENL//////////wEaDPdjJZ4N33LSEasCqCK0AWICGQecCHylzjVRgR7PcpXSslhsi9MZzJwYOhVAbNU+d6VKtuGTlN01EH2MXI9tICIr3eed3K7c2VfOZ6UuYuas+2Xv4f2tpTnHG0owW2+rbe6a11HZ2xOJcBoxQ/Z3meIuNhm3yiTCEsNYvSJydxPPJY4B9php43EzNesO3w6F0ZGr2EYrNXmZRJq+1yQoMxitXZga3Wg/FgMmrlurFyjXrR2zoZX+5va5Rxf+bgS2Fzql9CiIl86TBjItOsTnohJJM8R4+0BAIa4lxAKpFAkIkIvCCB/yWuamerauXWlgAcExlm3LjoyM"
+  token = ""
 
   region = "us-east-1"
 
 }
-
